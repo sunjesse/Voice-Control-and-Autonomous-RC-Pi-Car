db8f774ac0530b65ce81b23606718ea5972cbfb4
Jesse Sun <jessera@outlook.com>
2018-11-14 13:38:27 -0500

    Trying to stream video

--- process.py
+++ process.py
@@ -1,13 +1,41 @@
 import paho.mqtt.publish as publish
-import random
 import keyboard
-import zmq
-import cv2 as cv
 import numpy as np
-import base64
+import socket
+import struct
+from PIL import Image
+import cv2
+import threading
+import subprocess
 
+running = 0
+
+def read():
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', 8002))
+    server_socket.listen(0)
+
+    connection = server_socket.accept()[0].makefile('rb')
+    try:
+
+        cmdline = ['vlc', '--demux', 'h264', '-']
+        player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+        while True:
+            data = connection.read(1024)
+            if not data:
+                break
+            player.stdin.write(data)
+
+    finally:
+        connection.close()
+        server_socket.close()
+        player.terminate()
 
 while True:
+    if running == 0:
+        running = 1
+        t = threading.Thread(target=read)
+        t.start()
     try:
         if keyboard.is_pressed('w'):
             publish.single("carbot/server", payload="Forward", hostname="m15.cloudmqtt.com", port=13613, auth={'username':"pi", 'password':"53418112"}, tls=None)
@@ -21,6 +49,5 @@ while True:
             publish.single("carbot/server", payload="Stop", hostname="m15.cloudmqtt.com", port=13613, auth={'username':"pi", 'password':"53418112"}, tls=None)
     except:
         break
-print("Published")
 
 #"cdgavfxf", 'password':"ORXi9WQE9XJB"
